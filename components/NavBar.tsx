7b85d3d0508fb5bee48f31a82a8ccd6b414c7daa
dngocson <dngocson12@gmail.com>
2023-06-27 22:17:24 +0700

    initial commit
    initial commit

---
+++ components/NavBar.tsx
@@ -0,0 +1,104 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+const NavBar = () => {
+  return (
+    <div className="fixed w-full h-20  shadow-xl z-[100]">
+      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+        <Image
+          priority
+          src="/logo.png"
+          width={50}
+          height={50}
+          alt="logo"
+          className="object-contain"
+        />
+        <div>
+          <ul className="hidden md:flex">
+            <Link href={"/"}>
+              <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+            </Link>
+            <Link href={"/"}>
+              <li className="ml-10 text-sm uppercase hover:border-b">About</li>
+            </Link>
+            <Link href={"/"}>
+              <li className="ml-10 text-sm uppercase hover:border-b">Skill</li>
+            </Link>
+            <Link href={"/"}>
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Project
+              </li>
+            </Link>
+            <Link href={"/"}>
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Contact
+              </li>
+            </Link>
+          </ul>
+          <div className="md:hidden">
+            <AiOutlineMenu size={25} />
+          </div>
+        </div>
+      </div>
+      <div className="fixed left-0 top-0 w-full h-screen bg-black/70">
+        <div className="fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-300">
+          <div>
+            <div className="flex w-full items-center justify-between">
+              <Image src="/logo.png" width={50} height={40} alt="logo" />
+              <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer">
+                <AiOutlineClose />
+              </div>
+            </div>
+            <div className="border-b border-gray-300 my-4">
+              <p className="w-[85%] md:w-[90%] py-4">
+                Let's build something legendary together
+              </p>
+            </div>
+            <div className="py-4 flex flex-col ">
+              <ul className="uppercase">
+                <Link href={"/"}>
+                  <li className="py-4 text-sm">Home</li>
+                </Link>
+                <Link href={"/"}>
+                  <li className="py-4 text-sm">About</li>
+                </Link>
+                <Link href={"/"}>
+                  <li className="py-4 text-sm">Skills</li>
+                </Link>
+                <Link href={"/"}>
+                  <li className="py-4 text-sm">Projects</li>
+                </Link>
+                <Link href={"/"}>
+                  <li className="py-4 text-sm">Contact</li>
+                </Link>
+              </ul>
+              <div className="pt-40">
+                <p className="uppercase tracking-widest text-[#5651e5]">
+                  Let's Connect
+                </p>
+                <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                    <FaLinkedinIn />
+                  </div>
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                    <FaLinkedinIn />
+                  </div>
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                    <FaLinkedinIn />
+                  </div>
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                    <FaLinkedinIn />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
