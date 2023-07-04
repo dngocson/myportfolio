d28aa388f7fc9c78a631baa59d82c80b880d785e
dngocson <dngocson12@gmail.com>
2023-07-04 14:51:48 +0700

    fix bug in a tag
    fix bug in a tag

--- app/pizzaCo/page.tsx
+++ app/pizzaCo/page.tsx
@@ -35,11 +35,7 @@ const Page = () => {
             will be stored on the provided API and can be retrieved at any time.
             Click the button below to view the demo.
           </p>
-          <a
-            href="https://pizzaco.vercel.app/menu"
-            target="_blank"
-            rel="noreferrer"
-          >
+          <a href="https://pizzaco.vercel.app" target="_blank" rel="noreferrer">
             <button className="px-8 py-2 mt-4 mr-8">DEMO</button>
           </a>
           <a
